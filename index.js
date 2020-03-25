af199fd0b0116fd5ad0f5719a025ef69a593a110
Jeremiah Claspell <jeremiah@james-enterprise.com>
2020-03-25 13:26:32 -0600

    final project

--- index.js
+++ index.js
@@ -1,35 +1,115 @@
-Question1: what is the capitol of Alabama?
-            choice 1) Montgomery; 
-            choice 2) Phoenix
-            choice 3) St. Louis
-            choice 4) Little Rock
+var store = [
+    {question: 'What is the capital of Alabama?', 1: 'Montgomery', 2: 'Phoenix', 3: 'St. Louis', 4: 'Little Rock', answer: 'Little Rock'},
+    {question: 'what is the capital of Colorado?', 1: 'Dover', 2: 'Boise', 3: 'Denver', 4: 'Indianapolis', answer: 'Denver'},
+    {question: 'what is the capital of Missouri?', 1: 'Topeka', 2: 'Pierre', 3: 'Columbus', 4: 'Jefferson City', answer: 'Jefferson City'},
+    {question: 'What is the capital of Rhode Island?', 1: 'Montgomery', 2: 'Juno', 3: 'Providence', 4: 'Lincoln', answer: 'Providence'},
+    {question: 'What is the capital of South Carolina?', 1: 'Columbia', 2: 'Albany', 3: 'Boston', 4: 'Indianapolis', answer: 'Columbia'}
+]; 
+var correctCounter = 0; 
+var currentQuestionNumber =0; 
 
-Question2: what is the capitol of Colorado?
-            choice 1) Dover; 
-            choice 2) Boise; 
-            choice 3) Denver; 
-            choice 4) Indianapolis; 
 
-Question3: what is the capitol of Missouri?
-            choice 1) Topeka; 
-            choice 2) Pierre; 
-            choice 3) Columbus; 
-            choice 4) Jefferson City; 
+function startQuiz (){
+    $('.beginQuiz').on('click', '#beginQuizButton', function (event){ 
+        event.preventDefault(); 
+        $('.beginQuiz').addClass('hidden'); 
+        generateQuestions(); 
+        $('#0').removeClass('hidden');
+        $('.questionNumber').removeClass('hidden').text(`You're on question ${currentQuestionNumber+1}/5`)
+        $('.statsContainer').removeClass('hidden')
+    })
+}
 
-Question4: what is the capitol of Rhode Island?
-            choice 1) Montgomery; 
-            choice 2) Juno;
-            choice 3) Providence;	
-            choice 4) Lincoln;	
+function refreshQuestionNumber(){
+    $('.questionNumber').empty().text(`You're on question ${currentQuestionNumber+1}/5`)
 
-Question5: what is the capitol of South Carolina?
-            choice 1) Columbia; 
-            choice 2) Albany; 
-            choice 3) Boston; 
-            choice 4) Indianapolis; 
+}
 
-Question6: what is the capitol of Wyoming?
-            choice 1) Phoenix; 
-            choice 2) Pierre; 
-            choice 3) Cheyenne; 
-            choice 4) Juneau;             
+function refreshScore(){
+    $('.currentScore').empty().text(`Your score is ${correctCounter}/${currentQuestionNumber+1}`)
+}
+
+function generateQuestions (){
+    for (let i = 0; i<store.length; i++) {
+        $('main').append(
+            `<div class="inQuiz"> 
+                <form id="${i}" class="hidden questionForm">
+                    <label class="question">${store[i].question}</label>
+                    <fieldset>
+                        <label><input name="capital" id="capital1" type="radio" required value="${store[i][1]}">${store[i][1]}</label>
+                        <label><input name="capital" id="capital2" type="radio" required value="${store[i][2]}">${store[i][2]}</label>
+                        <label><input name="capital" id="capital3" type="radio" required value="${store[i][3]}">${store[i][3]}</label>
+                        <label><input name="capital" id="capital4" type="radio" required value="${store[i][4]}">${store[i][4]}</label>
+                        <button type="submit" id="submitAnswer" class="submitAnswer">Check Answer</button>
+                    </fieldset>
+                </form>
+            </div>`
+        )
+    }
+}
+
+function checkAnswer () {
+    $('main').on('submit', '.questionForm', function(event){
+        event.preventDefault(); 
+        if( store[$(this).attr('id')].answer === $(this).parent().find('input:checked').val() ){
+            correctCounter++; 
+            $('.questionResultContainer').addClass('true').removeClass('hidden'); 
+            $('.questionResult').text('True'); 
+        }  else {
+            $('.questionResultContainer').addClass('false').removeClass('hidden'); 
+            $('.questionResult').text('False'); 
+            $(`#${currentQuestionNumber}`).find(`label:contains(${store[currentQuestionNumber].answer})`).css("border", "3px solid green"); 
+        }; 
+        $(this).find('.submitAnswer').text('Next Question').parent().parent().addClass('nextQuestion').removeClass('questionForm'); 
+        $(this).removeClass('submitAnswer');  
+        $('.currentScore').removeClass('hidden'); 
+        refreshScore(); 
+    })
+}
+
+function nextQuestion () {
+    $('main').on('submit', '.nextQuestion', function(event){
+        event.preventDefault(); 
+        $('.nextQuestion').removeClass('nextQuestion');
+        $(this).addClass('submitAnswer') 
+        if( store[$(this).attr('id')].answer === $(this).find('input:checked').val() ){
+            $('.questionResultContainer').removeClass('true').addClass('hidden'); 
+            $('.questionResult').empty(); 
+        }  else {
+            $('.questionResultContainer').removeClass('false').addClass('hidden'); 
+            $('.questionResult').empty(); 
+        }; 
+        $(`#${currentQuestionNumber}`).addClass('hidden'); 
+        currentQuestionNumber++; 
+        $(`#${currentQuestionNumber}`).removeClass('hidden');
+        refreshQuestionNumber(); 
+
+        if(currentQuestionNumber===5){
+            $('main').addClass('hidden');
+            $('.questionNumber').addClass('hidden');
+            $('.currentScore').addClass('finishedQuiz');
+            $('#startNewQuiz').removeClass('hidden');
+        }
+    });
+}
+
+function startNewQuiz(){
+    $('header').on('click','#startNewQuiz', function(event){
+        $('.inQuiz').remove(); 
+        $('.beginQuiz').removeClass('hidden'); 
+        $('main').removeClass('hidden'); 
+        currentQuestionNumber=0; 
+        correctCounter=0; 
+        $('#startNewQuiz').addClass('hidden'); 
+        $('.currentScore').addClass('hidden').removeClass('finishedQuiz')
+    });
+}
+
+function handleEvents (){
+    startQuiz(); 
+    checkAnswer(); 
+    nextQuestion(); 
+    startNewQuiz(); 
+}
+
+$(handleEvents)
